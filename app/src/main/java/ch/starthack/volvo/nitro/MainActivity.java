68e8a7b8036c8a553a175eb6e0970d615f8ee3e0
Simon Bienz <simon.bienz@triboni.com>
2018-02-24 02:04:30 +0100

    [ADD] simple player list

--- app/src/main/java/ch/starthack/volvo/nitro/MainActivity.java
+++ app/src/main/java/ch/starthack/volvo/nitro/MainActivity.java
@@ -6,11 +6,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Pair;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView mainListView ;
+    private PlayerAdapter listAdapter ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +41,22 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Find the ListView resource.
+        mainListView = (ListView) findViewById( R.id.mainListView );
+
+        // Create and populate a List of planet names.
+        String[] players = new String[] { "Bob", "Tom", "Alice", "Luke"};
+        List<Pair<String, Integer>> playerList = new ArrayList<Pair<String, Integer>>();
+        for (String player : players){
+            playerList.add(new Pair<String, Integer>(player, ThreadLocalRandom.current().nextInt(30, 50 + 1)));
+        }
+
+        // Create ArrayAdapter using the player list.
+        listAdapter = new PlayerAdapter(this.getApplicationContext(), playerList);
+
+        // Set the ArrayAdapter as the ListView's adapter.
+        mainListView.setAdapter( listAdapter );
     }
 
     @Override
--- app/src/main/java/ch/starthack/volvo/nitro/PlayerAdapter.java
+++ app/src/main/java/ch/starthack/volvo/nitro/PlayerAdapter.java
@@ -1,8 +1,72 @@
 package ch.starthack.volvo.nitro;
 
-/**
- * Created by triboni on 24.02.18.
- */
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
-public class PlayerAdapter {
+import java.util.List;
+
+import ch.starthack.volvo.nitro.R;
+
+public class PlayerAdapter extends BaseAdapter {
+
+    private final Context mContext;
+    private List<Pair<String, Integer>> players;
+
+    public PlayerAdapter(Context context, List<Pair<String, Integer>> players) {
+        mContext = context;
+        this.players = players;
+    }
+
+    static class ViewHolder {
+        public TextView playerName;
+        public TextView playerScore;
+    }
+
+    @Override
+    public int getCount() {
+        return players.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return players.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return players.get(position).hashCode();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.rowitem, parent, false);
+            holder = new ViewHolder();
+            holder.playerName = (TextView) convertView.findViewById(R.id.list_player_name);
+            holder.playerScore = (TextView) convertView.findViewById(R.id.list_player_score);
+
+            convertView.setTag(holder);
+
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Pair<String, Integer> item = players.get(position);
+
+        holder.playerName.setText(item.first);
+        holder.playerName.setTextColor(Color.BLACK);
+        holder.playerScore.setText(item.second.toString());
+        holder.playerScore.setTextColor(Color.BLACK);
+
+        return convertView;
+    }
 }
